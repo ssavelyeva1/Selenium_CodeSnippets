b862c455e2d0e42afaa5770a4f9740f4f0c0f154
Aliaksandra Savelyeva <aliaksandra.savelyeva@gmail.com>
2023-05-31 15:10:29 +0200

    Create NetworkSpeed.java

---
+++ chrome_devtools_protocol/NetworkSpeed.java
@@ -0,0 +1,33 @@
+import java.util.Optional;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v113.network.Network;
+import org.openqa.selenium.devtools.v113.network.model.ConnectionType;
+
+public class NetworkSpeed {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver",
+				"E:\\learning\\Selenium WebDriver course\\Webdriver\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+
+		DevTools devTools = driver.getDevTools();
+		devTools.createSession();
+
+		devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+		devTools.send(
+				Network.emulateNetworkConditions(false, 3000, 20000, 10000, Optional.of(ConnectionType.ETHERNET)));
+
+		long startTime = System.currentTimeMillis();
+		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+		driver.findElement(By.cssSelector("button[routerlink*='library']")).click();
+		long endTime = System.currentTimeMillis();
+
+		System.out.println(endTime - startTime);
+
+	}
+
+}
