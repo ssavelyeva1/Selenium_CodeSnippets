db79a6e8850d5313e09244b3dbd781afb3236e61
Aliaksandra Savelyeva <aliaksandra.savelyeva@gmail.com>
2023-06-06 14:23:41 +0200

    Update FileUpload.java

--- AutoIT/FileUpload.java
+++ AutoIT/FileUpload.java
@@ -1,8 +1,12 @@
+import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FileUpload {
 
@@ -16,8 +20,20 @@ public class FileUpload {
 		driver.findElement(By.cssSelector("[class*='file_browse']")).click();
 		Thread.sleep(3000);
 
-		// calling autoit exe script
+		// calling autoit exe script to upload the file
 		Runtime.getRuntime().exec("C:\\Users\\AliaksandraSavelyeva\\Documents\\check\\fileupload.exe");
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(10000));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#submit_btn")));
+		driver.findElement(By.cssSelector("#submit_btn")).click();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[class='btn btn-primary btn-sm']")));
+		driver.findElement(By.cssSelector("a[class='btn btn-primary btn-sm']")).click();
+
+		File f = new File("C:\\Users\\AliaksandraSavelyeva\\Downloads\\P1010082.jpg");
+		if (f.exists()) {
+			System.out.println("File was successfully downloaded");
+		}
 
 	}
 
